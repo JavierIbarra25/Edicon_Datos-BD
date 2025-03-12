18269380a467a6b6d31c6b1044b1c77ea8483d6b
Javier_Ibarra <javiermaria.s463101@cesurformacion.com>
2025-03-12 13:45:55 +0100

    Update RRHH.sql

--- RRHH.sql
+++ RRHH.sql
@@ -136,12 +136,56 @@ ALTER TABLE EMPLEADOS
 DROP COLUMN Nacion
 
 -- 9. Introducir, como mínimo, dos tuplas en cada tabla. Para la tabla Cursos, debes introducir también estas tupla.
+
+INSERT INTO Empleados (NIF, Nombre, Apellido1, Apellido2, FecNacimiento, Salario, Sexo, Firma) 
+VALUES 
+('12345678A', 'Juan', 'García', 'López', '1985-05-15', 2500.00, 'HOMBRE', 'Firma1'),
+('87654321B', 'María', 'Martínez', 'Sánchez', '1990-08-22', 3000.00, 'MUJER', 'Firma2'),
+('98765432C', 'Carlos', 'Fernández', 'Gómez', '1988-03-10', 2800.00, 'HOMBRE', 'Firma3');
+
+INSERT INTO Capacitaciones (RefCurso, NIF_Empleado) 
+VALUES 
+(1, '12345678A'),
+(2, '87654321B'),
+(3, '98765432C');
+
+INSERT INTO Ediciones (CodEdicion, RefCurso, Fecha, Lugar, Coste, NIF_Docente) 
+VALUES 
+(101, 1, '2023-10-01', 'Madrid', 500.00, '12345678A'),
+(102, 2, '2023-11-01', 'Barcelona', 600.00, '87654321B'),
+(103, 3, '2023-12-01', 'Valencia', 450.00, '98765432C'),
+(106, 2, '2024-10-01', 'Madrid', 600.00,'12345678A');
+
+INSERT INTO Telefonos (NIF_Empleado, Telefono) 
+VALUES 
+('12345678A', '600123456'),
+('87654321B', '600654321'),
+('98765432C', '600987654');
+
+INSERT INTO Prerrequisitos (RefCursoARealizar, RefCursoRequisito, Tipo) 
+VALUES 
+(2, 1, 'OBLIGATORIO'),
+(3, 1, 'ACONSEJABLE'),
+(3, 2, 'OBLIGATORIO');
+
+INSERT INTO Matriculaciones (NIF_Alumno, CodEdicion) 
+VALUES 
+('12345678A', 101),
+('87654321B', 102),
+('98765432C', 103);
+
 INSERT INTO Cursos VALUES (1, 'GESTIÓN DE BASES DE DATOS', 100, 'GESTIÓN DE BASES DE DATOS');
 INSERT INTO Cursos VALUES (2, 'SISTEMAS OPERATIVOS', 200, 'IMPLANTACIÓN DE SISTEMAS OPERATIVOS');
 INSERT INTO Cursos VALUES (3, 'ADMON DE BASES DE DATOS', 60, 'ABD');
 
 -- Verificar las tuplas en la tabla Cursos
 SELECT * FROM Cursos;
+SELECT * FROM Empleados;
+SELECT * FROM Capacitaciones;
+SELECT * FROM Ediciones;
+SELECT * FROM Telefonos;
+SELECT * FROM Prerrequisitos;
+SELECT * FROM Matriculaciones;
 
 -- 10. Aumentar en 10 horas la duración de los cursos en cuyo título aparece “BASES DE DATOS”.
 
@@ -150,3 +194,11 @@ SET duracion = duracion + 10
 WHERE titulo LIKE '%BASES DE DATOS%';
 
 SELECT * FROM Cursos WHERE titulo LIKE '%BASES DE DATOS%';
+
+-- 11. Aumentar el sueldo en un 10% a los empleados que han impartido más de diez cursos (pueden ser de ediciones diferentes),utiliza GROUP BY en la tabla Ediciones para saber qué NIF_Docente ha 
+-- impartido 2 o más cursos. Luego aplica el UPDATE con un sub SELECT de lo anterior.
+
+SELECT NIF_Docente, count(*) as CursosImpartidos
+FROM Ediciones
+GROUP BY NIF_Docente
+HAVING count(*) >= 2;
